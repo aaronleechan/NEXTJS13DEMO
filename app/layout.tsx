8383bb480d8f92c538a247aa2bb8ad007666e162
aaron <aaron171091@gmail.com>
2022-11-03 07:38:23 -0700

    initialize

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable @next/next/no-head-element */
+
+import Link from "next/link";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body>
+        <main>
+          <nav>
+            <Link href="/">Home</Link>
+            <Link href="/notes">Notes</Link>
+          </nav>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ app/notes/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import CreateNote from './CreateNote';
+import styles from './Notes.module.css';
+
+const NotesPage =()=> {
+    return(
+        <CreateNote/>
+    )
+}
+
+export default NotesPage;
