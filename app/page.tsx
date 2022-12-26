da967647a02625afa1dd9927353a9494a7e936b0
aaron <aaron171091@gmail.com>
2022-12-26 13:49:07 -0800

    add Reference

--- app/page.tsx
+++ app/page.tsx
@@ -11,22 +11,33 @@ function Page() {
                 textDecoration: 'underline',
                 textUnderlineOffset: '0.5em'
             }} href={`/threeJS`} key={"basic"} passHref>
-                <h1>Geometry From Docs </h1>
+                <h1>Basic Geometry</h1>
             </Link>
+
             <hr/>
-            <Link style={{
+
+            <div>
+                <a target="_blank" href="https://www.youtube.com/watch?v=SV0W_B8iOzc&list=PLvfQp12V0hS3EbCBw7kDNOJ1l412tzcrM">Reference Youtube</a>
+                <Link style={{
                 textDecoration: 'underline',
                 textUnderlineOffset: '0.5em'
-            }} href={`/hashlips`} key={"hashlips"} passHref>
-                <h1>MetaVerse-HashLips From Youtube</h1>
-            </Link>
+                }} href={`/hashlips`} key={"hashlips"} passHref>
+                    <h1>MetaVerse-HashLips From Youtube</h1>
+                </Link>
+            </div>
+
             <hr/>
-            <Link style={{
+
+            <div>
+                <a target="_blank" href="https://www.udemy.com/course/threejs-using-react/">Reference Udemy</a>
+                <Link style={{
                 textDecoration: 'underline',
                 textUnderlineOffset: '0.5em'
-            }} href={`/showRoom`} key={"showRoom"} passHref>
-                <h1>Car Show Room From Udemy</h1>
-            </Link>
+                }} href={`/showRoom`} key={"showRoom"} passHref>
+                    <h1>Car Show Room</h1>
+                </Link>
+            </div>
+
         </div>
     )
 }
